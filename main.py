ad77567c5ddf8b331ac003b1ac0a373ecb832fc3
_ Exploit <watch.dog@qq.com>
2025-12-24 16:09:47 +0800

    测试接口

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.apis.sqlmap import router as sqlmap_router
+from app.apis.admin import router as admin_router
+from app.apis.db_health import router as db_router
 
 app = FastAPI(title="FastAPI SQLMap Manager")
+
+app.include_router(sqlmap_router)
+app.include_router(admin_router)
+app.include_router(db_router)
 
 @app.get("/")
 def root():
