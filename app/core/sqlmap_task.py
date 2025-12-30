456e8fd602eae7f445d0fbeee96066855ee24c02
_ Exploit <watch.dog@qq.com>
2025-12-30 17:51:15 +0800

    crud添加扫描任务信息

---
+++ app/core/sqlmap_task.py
@@ -0,0 +1,21 @@
+from app.database.database import AsyncSessionLocal
+from app.models.sqlmap_result import SqlmapScanPayload
+
+
+async def task_add(
+    task_id: str,
+    scan_url: str,
+    status: bool = False,
+    scan_risk: int = 1,
+    scan_level: int = 1,
+):
+    async with AsyncSessionLocal() as session:
+        task = SqlmapScanPayload(
+            task_id=task_id,
+            scan_url=scan_url,
+            status=status,
+            scan_risk=scan_risk,
+            scan_level=scan_level,
+        )
+        session.add(task)
+        await session.commit()
