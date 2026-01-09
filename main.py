d0145760beb0be42d882684655350db6eeef1f79
_ Exploit <watch.dog@qq.com>
2026-01-09 15:22:40 +0800

    加载celery的链接信息。否则router层识别不到

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.apis.sqlmap_api import router as sqlmap_router
 from app.apis.admin_api import router as admin_router
 from app.apis.db_health import router as db_router
+from app.middleware.celery_app import celery_app
 
 app = FastAPI(title="FastAPI SQLMap Manager")
 
@@ -12,4 +13,5 @@ app.include_router(db_router)
 
 @app.get("/")
 def root():
+    print("BROKER =", celery_app.conf.broker_url)
     return {"status": "running"}
